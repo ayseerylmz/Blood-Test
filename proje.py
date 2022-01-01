e2587acfc13343876c7eb1438f782cee92f84924
Ayşe Eryılmaz <80275512+ayseerylmz@users.noreply.github.com>
2022-01-01 19:33:38 +0300

    Create proje.py

---
+++ proje.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+diabetes = pd.read_csv('diabetes.csv')
+diabetes.columns 
+
+
+diabetes.head()
+
+print("Diabetes data set dimensions : {}".format(diabetes.shape))
+
+diabetes.groupby('Outcome').size()
+
+diabetes.groupby('Outcome').size()
+
+diabetes.groupby('Outcome').hist(figsize=(9, 9))
+
+diabetes.isnull().sum()
+diabetes.isna().sum()
+
+
+print("Total : ", diabetes[diabetes.BloodPressure == 0].shape[0])
+
+print(diabetes[diabetes.BloodPressure == 0].groupby('Outcome')['Age'].count())
+
+print("Total : ", diabetes[diabetes.Glucose == 0].shape[0])
+
+print(diabetes[diabetes.Glucose == 0].groupby('Outcome')['Age'].count())
+
+print("Total : ", diabetes[diabetes.SkinThickness == 0].shape[0])
+
+print(diabetes[diabetes.SkinThickness == 0].groupby('Outcome')['Age'].count())
+
+print("Total : ", diabetes[diabetes.BMI == 0].shape[0])
+
+print(diabetes[diabetes.BMI == 0].groupby('Outcome')['Age'].count())
+
+print("Total : ", diabetes[diabetes.Insulin == 0].shape[0])
+
+print(diabetes[diabetes.Insulin == 0].groupby('Outcome')['Age'].count())
+
+diabetes_mod = diabetes[(diabetes.BloodPressure != 0) & (diabetes.BMI != 0) & (diabetes.Glucose != 0)]
+print(diabetes_mod.shape)
+
+feature_names = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
+X = diabetes_mod[feature_names]
+y = diabetes_mod.Outcome
+
+from sklearn.model_selection import KFold
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import accuracy_score
+
+models = []
+
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('SVC', SVC(gamma='scale')))
+models.append(('LR', LogisticRegression(solver='lbfgs', max_iter=4000)))
+models.append(('DT', DecisionTreeClassifier()))
+models.append(('GNB', GaussianNB()))
+models.append(('RF', RandomForestClassifier(n_estimators=100)))
+models.append(('GB', GradientBoostingClassifier()))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify = diabetes_mod.Outcome, random_state=0)
+
+names = []
+scores = []
+
+for name, model in models:
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    scores.append(accuracy_score(y_test, y_pred))
+    names.append(name)
+
+tr_split = pd.DataFrame({'Name': names, 'Score': scores})
+print(tr_split)
+
+strat_k_fold = StratifiedKFold(n_splits=10, shuffle=True)
+
+names = []
+scores = []
+
+for name, model in models:
+    
+    score = cross_val_score(model, X, y, cv=strat_k_fold, scoring='accuracy').mean()
+    names.append(name)
+    scores.append(score)
+
+kf_cross_val = pd.DataFrame({'Name': names, 'Score': scores})
+print(kf_cross_val)
+
+axis = sns.barplot(x = 'Name', y = 'Score', data = kf_cross_val)
+axis.set(xlabel='Classifier', ylabel='Accuracy')
+
+for p in axis.patches:
+    height = p.get_height()
+    axis.text(p.get_x() + p.get_width()/2, height + 0.005, '{:1.4f}'.format(height), ha="center") 
+    
+plt.show()
+
+from sklearn.feature_selection import RFECV
+
+
+logreg_model = LogisticRegression(solver='lbfgs', max_iter=4000)
+
+rfecv = RFECV(estimator=logreg_model, step=1, cv=strat_k_fold, scoring='accuracy')
+rfecv.fit(X, y)
+
+plt.figure()
+plt.title('Logistic Regression CV score vs No of Features')
+plt.xlabel("Number of features selected")
+plt.ylabel("Cross validation score (nb of correct classifications)")
+plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_)
+plt.show()
+
+feature_importance = list(zip(feature_names, rfecv.support_))
+
+new_features = []
+
+for key,value in enumerate(feature_importance):
+    if(value[1]) == True:
+        new_features.append(value[0])
+        
+print(new_features)
+
+X_new = diabetes_mod[new_features]
+
+initial_score = cross_val_score(logreg_model, X, y, cv=strat_k_fold, scoring='accuracy').mean()
+print("Initial accuracy : {} ".format(initial_score))
+
+fe_score = cross_val_score(logreg_model, X_new, y, cv=strat_k_fold, scoring='accuracy').mean()
+print("Accuracy after Feature Selection : {} ".format(fe_score))
+
+gb_model = GradientBoostingClassifier()
+
+gb_rfecv = RFECV(estimator=gb_model, step=1, cv=strat_k_fold, scoring='accuracy')
+gb_rfecv.fit(X, y)
+
+plt.figure()
+plt.title('Gradient Boost CV score vs No of Features')
+plt.xlabel("Number of features selected")
+plt.ylabel("Cross validation score (nb of correct classifications)")
+plt.plot(range(1, len(gb_rfecv.grid_scores_) + 1), gb_rfecv.grid_scores_)
+plt.show()
+
+feature_importance = list(zip(feature_names, gb_rfecv.support_))
+
+new_features = []
+
+for key,value in enumerate(feature_importance):
+    if(value[1]) == True:
+        new_features.append(value[0])
+        
+print(new_features)
+
+
+X_new_gb = diabetes_mod[new_features]
+
+initial_score = cross_val_score(gb_model, X, y, cv=strat_k_fold, scoring='accuracy').mean()
+print("Initial accuracy : {} ".format(initial_score))
+
+fe_score = cross_val_score(gb_model, X_new_gb, y, cv=strat_k_fold, scoring='accuracy').mean()
+print("Accuracy after Feature Selection : {} ".format(fe_score))
+
+
+from sklearn.model_selection import GridSearchCV
+
+c_values = list(np.arange(1, 10))
+
+param_grid = [
+    {'C': c_values, 'penalty': ['l1'], 'solver' : ['liblinear'], 'multi_class' : ['ovr']},
+    {'C': c_values, 'penalty': ['l2'], 'solver' : ['liblinear', 'newton-cg', 'lbfgs'], 'multi_class' : ['ovr']}
+]
+
+grid = GridSearchCV(LogisticRegression(solver='lbfgs', max_iter=4000), param_grid, cv=strat_k_fold, scoring='accuracy')
+grid.fit(X_new, y)
+
+print(grid.best_params_)
+print(grid.best_estimator_)
+
+logreg_new = LogisticRegression(C=1, multi_class='ovr', penalty='l2', solver='liblinear')
+
+initial_score = cross_val_score(logreg_new, X_new, y, cv=strat_k_fold, scoring='accuracy').mean()
+print("Final accuracy : {} ".format(initial_score))
